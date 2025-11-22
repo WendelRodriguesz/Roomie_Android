e1f6acafcbd4af27ba7fd93c7d5da221ee8bfe9a
Werley <franciscowerley08@gmail.com>
2025-11-22 13:32:54 -0300

    ✨feat(backend): configura bucket R2 para upload de imagens

--- backend/src/main/java/com/project/roomie/ports/in/UsuarioInteressadoPortIn.java
+++ backend/src/main/java/com/project/roomie/ports/in/UsuarioInteressadoPortIn.java
@@ -1,8 +1,11 @@
 package com.project.roomie.ports.in;
 
 import com.project.roomie.core.model.UsuarioInteressado;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
 
 public interface UsuarioInteressadoPortIn {
 
     UsuarioInteressado cadastrar(UsuarioInteressado usuarioInteressado);
+    ResponseEntity<String> uploadFotoDePerfil(MultipartFile file, Integer IdUsuario);
 }
---
+++ backend/src/main/java/com/project/roomie/ports/out/BucketPortOut.java
@@ -0,0 +1,10 @@
+package com.project.roomie.ports.out;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface BucketPortOut {
+
+    String upload(MultipartFile file) throws IOException;
+}
