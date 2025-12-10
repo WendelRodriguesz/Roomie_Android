68e05ec9dbd1dd28e9a99ec9b347da75c179228f
Werley <franciscowerley08@gmail.com>
2025-12-10 01:06:31 -0300

    🔨refactor(backend): corrige encapsulamento do controller de usuário ofertante

--- backend/src/main/java/com/project/roomie/adapters/in/UsuarioOfertanteAdapterIn.java
+++ backend/src/main/java/com/project/roomie/adapters/in/UsuarioOfertanteAdapterIn.java
@@ -1,9 +1,9 @@
 package com.project.roomie.adapters.in;
 
-import com.project.roomie.core.service.UsuarioOfertanteService;
 import com.project.roomie.dto.create.UsuarioOfertanteCreateDTO;
 import com.project.roomie.dto.response.UsuarioOfertanteResponseDTO;
 import com.project.roomie.mapper.UsuarioOfertanteMapper;
+import com.project.roomie.ports.in.UsuarioOfertantePortIn;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,25 +12,25 @@ import org.springframework.web.multipart.MultipartFile;
 @RequestMapping("/api/usuarioOfertante")
 public class UsuarioOfertanteAdapterIn {
 
-    private final UsuarioOfertanteService usuarioOfertanteService;
+    private final UsuarioOfertantePortIn usuarioOfertantePortIn;
     private final UsuarioOfertanteMapper usuarioOfertanteMapper;
 
-    public UsuarioOfertanteAdapterIn(UsuarioOfertanteService usuarioOfertanteService,
+    public UsuarioOfertanteAdapterIn(UsuarioOfertantePortIn usuarioOfertantePortIn,
                                        UsuarioOfertanteMapper usuarioOfertanteMapper){
-        this.usuarioOfertanteService = usuarioOfertanteService;
+        this.usuarioOfertantePortIn = usuarioOfertantePortIn;
         this.usuarioOfertanteMapper = usuarioOfertanteMapper;
     }
 
     @PostMapping("/cadastrar")
     public UsuarioOfertanteResponseDTO cadastrar(@RequestBody UsuarioOfertanteCreateDTO usuarioOfertanteCreateDTO) {
         return usuarioOfertanteMapper.ModeltoResponseDTO(
-                usuarioOfertanteService.cadastrar(
+                usuarioOfertantePortIn.cadastrar(
                         usuarioOfertanteMapper.CreateDTOtoModel(
                                 usuarioOfertanteCreateDTO)));
     }
 
     @PostMapping("/uploadFotoDePerfil/{idUsuario}")
     public ResponseEntity<String> upload(@RequestParam("file") MultipartFile file, @PathVariable Integer idUsuario) {
-        return usuarioOfertanteService.uploadFotoDePerfil(file, idUsuario);
+        return usuarioOfertantePortIn.uploadFotoDePerfil(file, idUsuario);
     }
 }
