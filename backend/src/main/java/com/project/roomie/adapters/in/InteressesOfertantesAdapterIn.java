af4574cbb2413125bd3d787bdfe21b17ce725518
Camile <camileisi@alu.ufc.br>
2025-12-10 14:47:36 -0300

    :sparkles:feat(backend):adiciona adapters de interesses de ofertantes

---
+++ backend/src/main/java/com/project/roomie/adapters/in/InteressesOfertantesAdapterIn.java
@@ -0,0 +1,32 @@
+package com.project.roomie.adapters.in;
+
+import com.project.roomie.dto.create.InteressesOfertantesCreateDTO;
+import com.project.roomie.dto.response.InteressesOfertantesResponseDTO;
+import com.project.roomie.mapper.InteressesOfertantesMapper;
+import com.project.roomie.ports.in.InteressesOfertantesPortIn;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/interessesOfertantes")
+public class InteressesOfertantesAdapterIn {
+
+    private final InteressesOfertantesPortIn interessesOfertantesPortIn;
+    private final InteressesOfertantesMapper interessesOfertantesMapper;
+
+    @Autowired
+    public InteressesOfertantesAdapterIn(InteressesOfertantesPortIn interessesOfertantesPortIn,
+                                         InteressesOfertantesMapper interessesOfertantesMapper){
+        this.interessesOfertantesPortIn = interessesOfertantesPortIn;
+        this.interessesOfertantesMapper = interessesOfertantesMapper;
+    }
+
+    @PostMapping("/cadastrar/{id_usuario}")
+    public InteressesOfertantesResponseDTO cadastrar(@RequestBody InteressesOfertantesCreateDTO interessesOfertantesCreateDTO,
+                                                     @PathVariable Integer id_usuario ){
+        return interessesOfertantesMapper.ModeltoResponseDTO(
+                interessesOfertantesPortIn.cadastrarInteresses(
+                        interessesOfertantesMapper.CreateDTOtoModel(interessesOfertantesCreateDTO),
+                        id_usuario));
+    }
+}
---
+++ backend/src/main/java/com/project/roomie/adapters/out/InteressesOfertantesAdapterOut.java
@@ -0,0 +1,27 @@
+package com.project.roomie.adapters.out;
+
+import com.project.roomie.core.model.InteressesOfertantes;
+import com.project.roomie.infra.persistence.entity.InteressesOfertantesJpaEntity;
+import com.project.roomie.infra.persistence.repository.InteressesOfertantesRepository;
+import com.project.roomie.mapper.InteressesOfertantesMapper;
+import com.project.roomie.ports.out.InteressesOfertantesPortOut;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class InteressesOfertantesAdapterOut implements InteressesOfertantesPortOut {
+    private final InteressesOfertantesRepository interessesOfertantesRepository;
+    private final InteressesOfertantesMapper interessesOfertantesMapper;
+
+    @Autowired
+    public InteressesOfertantesAdapterOut(InteressesOfertantesRepository interessesOfertantesRepository,
+                                          InteressesOfertantesMapper interessesOfertantesMapper) {
+        this.interessesOfertantesRepository = interessesOfertantesRepository;
+        this.interessesOfertantesMapper = interessesOfertantesMapper;
+    }
+
+    @Override
+    public InteressesOfertantes save(InteressesOfertantesJpaEntity interessesOfertantesJpaEntity) {
+        return interessesOfertantesMapper.JpaEntitytoModel(interessesOfertantesRepository.save(interessesOfertantesJpaEntity));
+    }
+}
