6f68671e8b4970aae80377aacc5e81d240e1cef7
Camile <camileisi@alu.ufc.br>
2025-12-24 23:38:08 -0300

    :sparkles:feat(backend):adiciona metodo de atualização para usuario interessado na porta de entrada

--- backend/src/main/java/com/project/roomie/ports/in/UsuarioInteressadoPortIn.java
+++ backend/src/main/java/com/project/roomie/ports/in/UsuarioInteressadoPortIn.java
@@ -1,6 +1,8 @@
 package com.project.roomie.ports.in;
 
 import com.project.roomie.core.model.UsuarioInteressado;
+import com.project.roomie.dto.response.UsuarioInteressadoResponseDTO;
+import com.project.roomie.dto.update.UsuarioInteressadoUpdateDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -9,4 +11,5 @@ public interface UsuarioInteressadoPortIn {
     UsuarioInteressado cadastrar(UsuarioInteressado usuarioInteressado);
     ResponseEntity<String> uploadFotoDePerfil(MultipartFile file, Integer id_usuario);
     UsuarioInteressado visualizar(Integer id_usuario);
+    UsuarioInteressadoResponseDTO atualizar(Integer id, UsuarioInteressadoUpdateDTO usuarioInteressado);
 }
